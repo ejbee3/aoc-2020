7e2975c2db56ec30a0445aaf9c97adb4050d0280
Joe Baranski <joe.baranski49@gmail.com>
2020-12-03 15:24:28 -0500

    finished day two part one!

--- dayTwo/partOne.ts
+++ dayTwo/partOne.ts
@@ -1,3 +1,24 @@
 export default function dayTwoPartOne(array: string[]) {
-  return array
+
+  let count : number = 0
+  
+  const isValidPassword = (passwordStr: string) => {
+    const regexLetterReqArr : (RegExpMatchArray | null) = passwordStr.match(/[a-z]/)
+    const regexNumReqArr : (RegExpMatchArray | null) = passwordStr.match(/[0-9]+/g)
+    if (regexLetterReqArr != null && regexNumReqArr != null) {
+      const letterToMatch : string = regexLetterReqArr[0]
+      const passwordLetterCount : number = [...passwordStr.split(':')[1]].reduce((total, current) => current == letterToMatch ?
+        total += 1 : total, 0)
+      return passwordLetterCount >= Number(regexNumReqArr[0]) && 
+        passwordLetterCount <= Number(regexNumReqArr[1])
+  } 
+    return false
+  }
+  for (let i: number = 0; i < array.length; i++) {
+    if (isValidPassword(array[i])) {
+      count += 1
+    }
+  }
+  return count
 }
+  
